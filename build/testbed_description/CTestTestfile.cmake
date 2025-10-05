e84e48ad80d8e28b14564a2138f32d028ec3b6f7
helloboss105 <aniketnagare444@gmail.com>
2025-10-05 21:02:12 +0530

    Ignore build, install, and log directories

--- build/testbed_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hello-boss/assignment_ws/src/l1-helloboss105-main/testbed_description
-# Build directory: /home/hello-boss/assignment_ws/build/testbed_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
